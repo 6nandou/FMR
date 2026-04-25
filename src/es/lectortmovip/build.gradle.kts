d137165bd53278edddddf454ba6aa0ac41308da5
Feña <87620680+6nandou@users.noreply.github.com>
2026-04-24 22:53:30 -0400

    Update build.gradle.kts

--- src/es/lectortmovip/build.gradle.kts
+++ src/es/lectortmovip/build.gradle.kts
@@ -3,6 +3,15 @@ plugins {
     kotlin("android")
 }
 
+android {
+    namespace = "eu.kanade.tachiyomi.extension.es.lectortmovip"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+}
+
 val extName by extra("LectorTMO VIP")
 val pkgNameSuffix by extra("es.lectortmovip")
 val extClass by extra(".LectorTmovip")
@@ -11,4 +20,5 @@ val isNsfw by extra(true)
 
 dependencies {
     implementation("androidx.preference:preference:1.2.1")
+    implementation("org.jsoup:jsoup:1.17.2")
 }
